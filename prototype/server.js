22a684e44059324e170048e914db92b877a09a17
Taras Novak <taras.novak@randomfractals.com>
2016-09-05 05:34:31 -0500

    created simple express.js for serving data files via xhr

---
+++ prototype/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// __dirname will use the current path from where you run this file 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/src')));
+
+app.listen(8000);
+console.log('Listening on port 8000');
