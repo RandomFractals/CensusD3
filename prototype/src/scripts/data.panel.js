2e1d3c74c52db7805f4fa11b159461927097762d
Taras Novak <taras.novak@randomfractals.com>
2016-09-11 10:46:28 -0500

    created new data.panel.js UI component for census data and graphs display

---
+++ prototype/src/scripts/data.panel.js
@@ -0,0 +1,45 @@
+/**
+ * App data panel UI component.
+ **/ 
+function DataPanel() {
+
+  // data section title ref 
+  this.dataTitle = d3.select('#dataTitle');
+
+  // data image ref
+  this.dataImage = d3.select('#dataImage');
+
+  // data list section ref 
+  this.dataList = d3.select('#dataList');
+
+  // data graph section ref 
+  this.dataGraph = d3.select('#dataGraph');
+
+  // number format for display
+  this.numberFormat = d3.format(',');
+
+} // end of DataPanel() constructor
+
+
+/**
+ * Updates app data title, region/data type image, 
+ * data list and data graph with the selection region
+ * or census data type geo data.
+ */
+  console.log('DataPanel::update', listData, graphData);
+
+  // update data section title
+  this.dataTitle.text(title);
+
+  // update data section image   
+  this.dataImage.attr('src', '../images/flags/' + 
+    title.split(' ').join('_') + '.svg.png'); // convert spaces to _
+
+  // TODO: gen ul/li for list data display
+  this.dataList.text( JSON.stringify(listData) );
+
+  // TODO: create data graph for graph data display
+  this.dataGraph.text('[todo: create data.graph UI component]\n' +
+    JSON.stringify(graphData) );
+}
+
