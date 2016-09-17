06c2a775ce450b2f27d7af80c969b42aca4e22c1
Taras Novak <taras.novak@randomfractals.com>
2016-09-17 08:25:34 -0500

    added alpha get pop census data setup

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,53 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var async = require('async');
-
+var http = require('http');
 var CensusDataKey = process.env.CENSUS_DATA_API_KEY;
 
 // Expose tweets endpoint for ang2 app
-router.get('/app/population/:query?', function(request, response, next) {
+router.get('/census/population/:query?', function(request, response, next) {
   // inject search query from client
-  var query = 'USA';
+  var query = 'us';
   if (request.params.query) {
     query = request.params.query;
   }
   log('population query: ', query );
+  var year = 2015;  
+  var queryParams = {
+    year: year,
+    get: 'POP,GEONAME',
+    for: query,
+    key: CensusDataKey
+  }
+
+  // get census data
+  var requestPath = '/data/' + queryParams.year +
+                '/pep/population' +
+                '?get=' + queryParams.get +
+                '&for=' + queryParams.for + ':*' +
+                '&key=' + queryParams.key; 
+  http.request({
+        host: 'api.census.gov',
+        method: 'GET',
+        path: requestPath
+    }, onCensusDataResponse).end();
+
 });
 
 
+/**
+ * Processes census data service response.
+ */
+function onCensusDataResponse(response) {
+  var responseData = '';
+  response.on('data', function(chunk) {
+    responseData += chunk;
+  });
+
+  response.on('end', function() {
+    log('response: ', responseData);
+  });
+}
 
 /**
  * Quick obj log func.
