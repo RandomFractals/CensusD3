4d974537e1f83203a3300679471880a21c01c8c2
Taras Novak <taras.novak@randomfractals.com>
2017-11-12 10:22:00 -0600

    added quick get states population data with axios test to pop chart comp for now

--- vue/quasar/src/components/PopulationChart.vue
+++ vue/quasar/src/components/PopulationChart.vue
@@ -5,16 +5,50 @@
     </q-card-title>
     <q-card-separator />
     <q-card-main>
-      TODO: states/counties population chart
+      <div class="error-message" v-if="showError">
+       {{ errorMessage }}
+      </div>
     </q-card-main>
   </q-card>  
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  name: 'population-chart',
+  props: {},
+
   data () {
     return {
-      title: 'USA population'
+      title: 'USA population',
+      loaded: false,
+      populationData: [],
+      showError: false,
+      errorMessage: 'Error loading population data'
+    }
+  },
+
+  mounted () {
+    this.getPopulationData()
+  },
+
+  methods: {
+    resetState () {
+      this.loaded = false
+      this.showError = false
+    },
+    getPopulationData () {
+      axios.get(`http://censusd3.herokuapp.com/census/population/state:*`)
+        .then(response => {
+          console.log('getPopulationData:', response.data)
+          this.populationData = response.data
+          this.loaded = true
+        })
+        .catch(err => {
+          this.errorMessage = err.response.data.error
+          this.showError = true
+        })
     }
   }
 }
