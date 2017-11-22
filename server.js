9f1e47ab5fdf778c41b916a92ae953f36da955fc
Taras Novak <taras.novak@randomfractals.com>
2017-11-22 11:16:08 -0600

    changed server.js to point to new vue/quasar/dist for v.0.0.1 of census-vue dash app deployment

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'prototype/src')));
+app.use(express.static(path.join(__dirname, 'vue/quasar/dist'))); // 'prototype/src')));
 app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
