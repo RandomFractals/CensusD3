ce27d8c515f2e503c50e470e6f3d0625dee68c69
Taras Novak <taras.novak@randomfractals.com>
2017-11-24 10:35:11 -0600

    got rid of boilerplate quasar app code. added window resize hook to fill in later

--- vue/quasar/src/components/Dashboard.vue
+++ vue/quasar/src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout ref="layout"
     view="lHh Lpr fFf">
-
+    <q-window-resize-observable @resize="resize" />
     <!-- app toolbar -->
     <q-toolbar slot="header" class="app-toolbar">
       <q-btn flat @click="$refs.layout.toggleLeft()">
@@ -139,8 +139,6 @@
 
 <script>
 import {
-  dom,
-  event,
   openURL,
   QLayout,
   QToolbar,
@@ -153,30 +151,13 @@ import {
   QItemSide,
   QItemMain,
   QItemSeparator,
+  QWindowResizeObservable,
   Events
 } from 'quasar'
 
 import USAMap from './USAMap.vue'
 import PopulationTable from './PopulationTable.vue'
 import PopulationChart from './PopulationChart.vue'
-
-const
-  { viewport } = dom,
-  { position } = event,
-  moveForce = 30,
-  rotateForce = 40,
-  RAD_TO_DEG = 180 / Math.PI
-
-function getRotationFromAccel (accelX, accelY, accelZ) {
-  /* Reference: http://stackoverflow.com/questions/3755059/3d-accelerometer-calculate-the-orientation#answer-30195572 */
-  const sign = accelZ > 0 ? 1 : -1
-  const miu = 0.001
-
-  return {
-    roll: Math.atan2(accelY, sign * Math.sqrt(Math.pow(accelZ, 2) + miu * Math.pow(accelX, 2))) * RAD_TO_DEG,
-    pitch: -Math.atan2(accelX, Math.sqrt(Math.pow(accelY, 2) + Math.pow(accelZ, 2))) * RAD_TO_DEG
-  }
-}
 
 export default {
   name: 'index',
@@ -192,6 +173,7 @@ export default {
     QItemSide,
     QItemMain,
     QItemSeparator,
+    QWindowResizeObservable,
     usaMap: USAMap,
     PopulationTable,
     PopulationChart
@@ -211,26 +193,7 @@ export default {
       populationData: [],
       loaded: false,
       showError: false,
-      errorMessage: 'Error loading population data',
-      orienting: window.DeviceOrientationEvent && !this.$q.platform.is.desktop,
-      rotating: window.DeviceMotionEvent && !this.$q.platform.is.desktop,
-      moveX: 0,
-      moveY: 0,
-      rotateY: 0,
-      rotateX: 0
-    }
-  },
-
-  computed: {
-    position () {
-      const transform = `rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg)`
-      return {
-        top: this.moveY + 'px',
-        left: this.moveX + 'px',
-        '-webkit-transform': transform,
-        '-ms-transform': transform,
-        transform
-      }
+      errorMessage: 'Error loading population data'
     }
   },
 
@@ -260,53 +223,9 @@ export default {
       this.$census.getPopulation()
     },
 
-    // TODO: retrofit these for proper device rotation event handling
-    // and responsive UI layout and components sizing updates later
-    move (evt) {
-      const
-        {width, height} = viewport(),
-        {top, left} = position(evt),
-        halfH = height / 2,
-        halfW = width / 2
-
-      this.moveX = (left - halfW) / halfW * -moveForce
-      this.moveY = (top - halfH) / halfH * -moveForce
-      this.rotateY = (left / width * rotateForce * 2) - rotateForce
-      this.rotateX = -((top / height * rotateForce * 2) - rotateForce)
-    },
-    rotate (evt) {
-      if (evt.rotationRate &&
-          evt.rotationRate.beta !== null &&
-          evt.rotationRate.gamma !== null) {
-        this.rotateX = evt.rotationRate.beta * 0.7
-        this.rotateY = evt.rotationRate.gamma * -0.7
-      }
-      else {
-        /* evt.acceleration may be null in some cases, so we'll fall back
-           to evt.accelerationIncludingGravity */
-        const
-          accelX = evt.acceleration.x || evt.accelerationIncludingGravity.x,
-          accelY = evt.acceleration.y || evt.accelerationIncludingGravity.y,
-          accelZ = evt.acceleration.z || evt.accelerationIncludingGravity.z - 9.81,
-          rotation = getRotationFromAccel(accelX, accelY, accelZ)
-
-        this.rotateX = rotation.roll * 0.7
-        this.rotateY = rotation.pitch * -0.7
-      }
-    },
-    orient (evt) {
-      if (evt.beta === null || evt.gamma === null) {
-        window.removeEventListener('deviceorientation', this.orient, false)
-        this.orienting = false
-
-        window.addEventListener('devicemotion', this.rotate, false)
-      }
-      else {
-        this.rotateX = evt.beta * 0.7
-        this.rotateY = evt.gamma * -0.7
-      }
+    resize (size) {
+      console.log('dashboard:resize', size)
     }
-
   }, // end of methods
 
   /**
